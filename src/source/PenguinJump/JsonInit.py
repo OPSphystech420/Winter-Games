cbba21e2819c89f0556367ef561e23893948a49d
OPSphystech420 <116661459+OPSphystech420@users.noreply.github.com>
2022-12-07 06:44:24 +0300

    Add files via upload

---
+++ src/source/PenguinJump/JsonInit.py
@@ -0,0 +1,35 @@
+import json
+
+class Dict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+class Config(object):
+    @staticmethod
+    def __load__(data):
+        if type(data) is dict:
+            return Config.load_dict(data)
+        elif type(data) is list:
+            return Config.load_list(data)
+        else:
+            return data
+
+    @staticmethod
+    def load_dict(data: dict):
+        result = Dict()
+        for key, value in data.items():
+            result[key] = Config.__load__(value)
+        return result
+
+    @staticmethod
+    def load_list(data: list):
+        result = [Config.__load__(item) for item in data]
+        return result
+
+    @staticmethod
+    def load_json(path: str):
+        with open(path, "r") as f:
+            result = Config.__load__(json.loads(f.read()))
+        return result
